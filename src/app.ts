ccff4da8ab69f0817f9a23b49287ea413a270cef
[Harshit Gupta] <[hrstgpt87@gmail.com]>
2026-01-29 12:47:48 +0530

    Trial

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,27 @@
+// const exp=require('express')
+const application=require('./server')
+
 interface App_Interface{
     startServer():void
     connectDB():void
     initializeRoutes():void;
+}
+
+class App implements App_Interface{
+    port:number
+    app:Express.Application
+
+    constructor(){
+        this.port=4000
+        this.app=application
+    }
+    startServer(): void {
+        
+    }
+    connectDB(): void {
+        
+    }
+    initializeRoutes(): void {
+        
+    }
 }
--- src/server.ts
+++ src/server.ts
@@ -7,3 +7,5 @@ const app=express()
 app.listen(3000,()=>{
     console.log("Server is running")
 })
+
+module.exports=app
